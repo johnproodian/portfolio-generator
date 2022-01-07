6ff0c7422321019aa4e9aecf254ad937c86ba7bf
Jotas <jproodian721@gmail.com>
2022-01-07 11:41:47 -0600

    added inquirer package and inquirer question object to ask for a name

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs
-
+// const pageHTML = generatePage(name, github);
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
 
-    console.log('Portfolio complete! Checkout out index.html to see the output!');
-});
-// about 2/3 thru 9.2.5
+
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Checkout out index.html to see the output!');
+// });
+
+inquirer   
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+
+
+
+
+
+
+
+
+
+
+// about 1/4 the way thru 9.3.5 "Import Inquirer into app.js"
